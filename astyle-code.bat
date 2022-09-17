b2eac7fe36335e754721e802cc6e94256dad96a5
Robert Beckebans <robert.beckebans@gmail.com>
2022-09-17 13:19:43 +0200

    Exclude third parties from formatting

--- astyle-code.bat
+++ astyle-code.bat
@@ -1,13 +1,13 @@
-astyle.exe -v --formatted --options=astyle-options.ini --exclude="donut/thirdparty" --recursive *.h
-astyle.exe -v --formatted --options=astyle-options.ini --exclude="donut/thirdparty" --recursive *.c
-astyle.exe -v --formatted --options=astyle-options.ini --exclude="donut/thirdparty" --recursive *.cpp
-astyle.exe -v --formatted --options=astyle-options.ini --exclude="donut/thirdparty" --recursive *.glsl
-astyle.exe -v --formatted --options=astyle-options.ini --exclude="donut/thirdparty" --recursive *.vert
-astyle.exe -v --formatted --options=astyle-options.ini --exclude="donut/thirdparty" --recursive *.frag
-astyle.exe -v --formatted --options=astyle-options.ini --exclude="donut/thirdparty" --recursive *.tesc
-astyle.exe -v --formatted --options=astyle-options.ini --exclude="donut/thirdparty" --recursive *.tese
-astyle.exe -v --formatted --options=astyle-options.ini --exclude="donut/thirdparty" --recursive *.comp
-astyle.exe -v --formatted --options=astyle-options.ini --exclude="donut/thirdparty" --recursive *.hlsl
-astyle.exe -v --formatted --options=astyle-options.ini --exclude="donut/thirdparty" --recursive *.hlsli
+astyle.exe -v --formatted --options=astyle-options.ini --exclude="thirdparty" --exclude="rtxmu" --recursive *.h
+astyle.exe -v --formatted --options=astyle-options.ini --exclude="thirdparty" --exclude="rtxmu" --recursive *.c
+astyle.exe -v --formatted --options=astyle-options.ini --exclude="thirdparty" --exclude="rtxmu" --recursive *.cpp
+astyle.exe -v --formatted --options=astyle-options.ini --exclude="thirdparty" --exclude="rtxmu" --recursive *.glsl
+astyle.exe -v --formatted --options=astyle-options.ini --exclude="thirdparty" --exclude="rtxmu" --recursive *.vert
+astyle.exe -v --formatted --options=astyle-options.ini --exclude="thirdparty" --exclude="rtxmu" --recursive *.frag
+astyle.exe -v --formatted --options=astyle-options.ini --exclude="thirdparty" --exclude="rtxmu" --recursive *.tesc
+astyle.exe -v --formatted --options=astyle-options.ini --exclude="thirdparty" --exclude="rtxmu" --recursive *.tese
+astyle.exe -v --formatted --options=astyle-options.ini --exclude="thirdparty" --exclude="rtxmu" --recursive *.comp
+astyle.exe -v --formatted --options=astyle-options.ini --exclude="thirdparty" --exclude="rtxmu" --recursive *.hlsl
+astyle.exe -v --formatted --options=astyle-options.ini --exclude="thirdparty" --exclude="rtxmu" --recursive *.hlsli
 
 pause

08ad973e24fecf4369c9306b665281f95e74d7d3
Robert Beckebans <robert.beckebans@gmail.com>
2022-09-17 13:10:38 +0200

    Added astyle

---
+++ astyle-code.bat
@@ -0,0 +1,13 @@
+astyle.exe -v --formatted --options=astyle-options.ini --exclude="donut/thirdparty" --recursive *.h
+astyle.exe -v --formatted --options=astyle-options.ini --exclude="donut/thirdparty" --recursive *.c
+astyle.exe -v --formatted --options=astyle-options.ini --exclude="donut/thirdparty" --recursive *.cpp
+astyle.exe -v --formatted --options=astyle-options.ini --exclude="donut/thirdparty" --recursive *.glsl
+astyle.exe -v --formatted --options=astyle-options.ini --exclude="donut/thirdparty" --recursive *.vert
+astyle.exe -v --formatted --options=astyle-options.ini --exclude="donut/thirdparty" --recursive *.frag
+astyle.exe -v --formatted --options=astyle-options.ini --exclude="donut/thirdparty" --recursive *.tesc
+astyle.exe -v --formatted --options=astyle-options.ini --exclude="donut/thirdparty" --recursive *.tese
+astyle.exe -v --formatted --options=astyle-options.ini --exclude="donut/thirdparty" --recursive *.comp
+astyle.exe -v --formatted --options=astyle-options.ini --exclude="donut/thirdparty" --recursive *.hlsl
+astyle.exe -v --formatted --options=astyle-options.ini --exclude="donut/thirdparty" --recursive *.hlsli
+
+pause
